a801ceafc4662c5060e3d121ac793ff9f542336b
Lillian Paris <lillian.paris529@gmail.com>
2021-01-14 21:57:50 -0700

    Started navbar creation

---
+++ client/src/pages/Home.js
@@ -0,0 +1,21 @@
+// import Navbar from 'react-bootstrap/Navbar';
+import Menu from '../components/Navbar'
+
+function Home() {
+
+    return (
+        
+        <div>
+            <Menu />
+            <h2>
+                Weffle
+            </h2>
+            <p>
+                Welcome Home
+            </p>
+        </div>
+    );
+}
+
+export default Home;
+
