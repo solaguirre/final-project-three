900b6655b278e88f091bf99435566666f441e8e3
orhiley90 <69226157+orhiley90@users.noreply.github.com>
2021-01-30 10:46:36 -0700

    switching branches

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.html';
