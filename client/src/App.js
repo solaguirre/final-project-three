73546ded257af614ebc666760fedd2b575c59561
Lillian Paris <lillian.paris529@gmail.com>
2021-02-01 19:02:20 -0700

    Fixed navbar and background

--- client/src/App.js
+++ client/src/App.js
@@ -39,7 +39,7 @@ function App() {
 
     return (
         <Router>
-            <Menu></Menu>
+            <Menu/>
             <Switch>
                 <Route exact path='/'>
                     <Home />
